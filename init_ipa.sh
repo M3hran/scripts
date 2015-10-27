199fbb44812371e0ddec38745bc968d3b8d0e500
M3hran <M3hran@gmail.com>
2015-10-27 15:43:21 -0400

    tweaks

--- init_ipa.sh
+++ init_ipa.sh
@@ -4,15 +4,42 @@
 # it should be run after installation of IPA client
 # Maintainer: M3hran
 
-FLAG=true;
+DOMAIN=true;
+STATIC=true;
 
-function check {
+function if_check {
+	if grep -q static "/etc/network/interfaces"; then
+		STATIC=true;
+	else
+		STATIC=false;
+	fi
+
+}
+
+
+function hostname_check {
 
 	if [[ $(hostname -f) == *.scientiamobile.local ]]; then
-		FLAG=false;
-		return 0;
+		DOMAIN=true;
+	else
+		DOMAIN=false;
 	fi
 }
+
+if_check
+
+if [ "$STATIC" == false ]; then
+
+	printf "\nERROR: NIC must be statically defined.\n\n"
+	exit
+fi
+
+hostname_check
+
+apt-get update
+apt-get -y install 'freeipa-client'
+ipa-client-install
+
 
 
 sed -i "\$a session     optional      pam_mkhomedir.so skel=/etc/skel umask=077" /etc/pam.d/common-auth
@@ -21,9 +48,8 @@ sed -Ei 's/^(.*services.*)/services = nss, pam, ssh, sudo/g' /etc/sssd/sssd.conf
 sed -i "\$a # configure SUDO and GSSAPI authentication\nsudo_provider = ldap\nldap_uri = ldap://id.scientiamobile.com\nldap_sudo_search_base = ou=sudoers,dc=scientiamobile,dc=local\nldap_sasl_mech = GSSAPI\nldap_sasl_authid = host/$(hostname -f)\nldap_sasl_realm = SCIENTIAMOBILE.LOCAL\nkrb5_server = id.scientiamobile.com\n" /etc/sssd/sssd.conf 
 echo "SSSD configured..."
 
-check
 
-if [ "$FLAG" == true ]; then
+if [ "$FLAG" == false ]; then
 	sed -i 's/exit 0//g' /etc/rc.local
 	sed -i "\$a nisdomainname scientiamobile.local"  /etc/rc.local
 	sed -i "\$a service sssd restart"  /etc/rc.local
@@ -31,4 +57,4 @@ if [ "$FLAG" == true ]; then
 	echo "nisdomainname added..."
 fi
 
-echo "Done."
+printf "Done.\n\n"
