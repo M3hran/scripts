2e40721d90bbc33127da9e8639cdbb5068c8227f
M3hran <M3hran@gmail.com>
2015-10-27 15:56:55 -0400

    tweaks

--- init_ipa.sh
+++ init_ipa.sh
@@ -7,6 +7,7 @@
 DOMAIN=true;
 STATIC=true;
 
+#dirty check to see if NIC is statically set
 function if_check {
 	if grep -q static "/etc/network/interfaces"; then
 		STATIC=true;
@@ -16,7 +17,7 @@ function if_check {
 
 }
 
-
+#check if tld is scientiamobile.local
 function hostname_check {
 
 	if [[ $(hostname -f) == *.scientiamobile.local ]]; then
@@ -48,13 +49,20 @@ sed -Ei 's/^(.*services.*)/services = nss, pam, ssh, sudo/g' /etc/sssd/sssd.conf
 sed -i "\$a # configure SUDO and GSSAPI authentication\nsudo_provider = ldap\nldap_uri = ldap://id.scientiamobile.com\nldap_sudo_search_base = ou=sudoers,dc=scientiamobile,dc=local\nldap_sasl_mech = GSSAPI\nldap_sasl_authid = host/$(hostname -f)\nldap_sasl_realm = SCIENTIAMOBILE.LOCAL\nkrb5_server = id.scientiamobile.com\n" /etc/sssd/sssd.conf 
 echo "SSSD configured..."
 
-
+#add nisdomain if tld is different than default for IPA
 if [ "$FLAG" == false ]; then
 	sed -i 's/exit 0//g' /etc/rc.local
 	sed -i "\$a nisdomainname scientiamobile.local"  /etc/rc.local
 	sed -i "\$a service sssd restart"  /etc/rc.local
 	sed -i "\$a exit 0"  /etc/rc.local
 	echo "nisdomainname added..."
+fi
+
+#remove ubuntu account if it exits
+if grep -q ubuntu "/etc/passwd" ; then
+	printf "Removing Ubuntu account..\n"
+	userdel ubuntu
+	rm -rf /home/ubuntu
 fi
 
 printf "Done.\n"
