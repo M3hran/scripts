8ddb70215a6189d9507ead514807ef93eef7edd9
M3hran <M3hran@gmail.com>
2015-11-06 13:17:18 -0500

    zabbix scripted

---
+++ init_zabbix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "downloading zabbix-agent 2.2.1"
+wget http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+trusty_all.deb
+dpkg -i zabbix-release_2.2-1+trusty_all.deb
+apt-get update
+apt-get install zabbix-agent
+
+
+echo "changing zabbix-agent conf file"
+sed -i 's/LogFileSize=0/LogFileSize=500/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Server=127.0.0.1/Server=zabbix.scientiamobile.local/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix.scientiamobile.local/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Hostname=Zabbix server/Hostname='$HOSTNAME'/' /etc/zabbix/zabbix_agentd.conf
+service zabbix-agent restart
+echo "done"
