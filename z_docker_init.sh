6aaa9923b5a2c944e96c565d98cada4b2f984a20
M3hran <M3hran@gmail.com>
2017-06-17 11:42:03 -0400

    Update z_docker_init.sh

--- z_docker_init.sh
+++ z_docker_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 curl -sSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.12.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.12.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/sbin/docker-compose
+chmod +x /usr/sbin/docker-compose
 systemctl enable docker
 systemctl start docker
 
