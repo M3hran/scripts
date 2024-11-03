38d7fb6a9b5b9c60e05936f9280b27174f0589aa
M3hran <m3hran@gmail.com>
2024-11-03 14:08:45 -0500

    apt git install

--- git_init.sh
+++ git_init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-#ensure ssh_keys are available in ~/.ssh and github account
-#ensure repo url is set to ssh format ex.
+#
+##ensure ssh_keys are available in ~/.ssh and github account
+##ensure repo url is set to ssh format ex.
 #git remote set-url origin git@github.com:M3hran/scripts.git
+#
+apt update > /dev/null 2>&1
+apt install -y git
 
 git config --global user.name M3hran
 git config --global user.email m3hran@gmail.com
@@ -10,3 +13,4 @@ git config --global color.ui auto
 git config --global init.defaultBranch master
 
 ssh -T git@github.com
+#
